e831ff028a9760d9e54a26f165bfb6fead6fb399
ushan pramod <125153601+ushandissanayaka@users.noreply.github.com>
2024-09-08 19:20:03 +0530

    frontend setup

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+
+
+function Navbar() {
+  return (
+    <nav>
+   <h1>This is navbar</h1>
+    </nav>
+  )
+}
+
+export default Navbar;
---
+++ client/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#695B5B",
+        "secondary": "#AD63E8",
+        "tartiary": "#A17EED",
+        "pink": "#F6A4E9"
+
+      }
+    },
+  },
+  plugins: [],
+}
+
