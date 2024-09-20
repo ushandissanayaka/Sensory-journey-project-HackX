7eedc2ff9df7be9262a6a2f1c641a7a7c3be246f
IllangasingheIMDP <157776370+IllangasingheIMDP@users.noreply.github.com>
2024-09-20 17:49:50 +0530

    Update GameView.jsx

--- client/src/components/Games/GameView.jsx
+++ client/src/components/Games/GameView.jsx
@@ -10,7 +10,7 @@ export const GameView = () => {
         <div id='card-area'>
   <div className='wrapper '>
     
-      <GameCard GameName={'Color Safari'} GameDescription={'Join and color animals and feed the fuckers and huuiiyaaa hale haleee'} GamePhoto={GameCard1} Tag1={'Visual'} Tag2={'Pattern Recognition'}></GameCard>
+      <GameCard GameName={'Color Safari'} GameDescription={'Join and color animals and feed them'} GamePhoto={GameCard1} Tag1={'Visual'} Tag2={'Pattern Recognition'}></GameCard>
       <GameCard GameName={'Scribbler Splash'} GameDescription={'Unleash your creativity with vibrant strokes and playful textures on a blank canvas!'} GamePhoto={GameCard2} Tag1={'Creativity'} Tag2={'Fine Motor Skills'}></GameCard>
       <GameCard GameName={'Bubble Pop'} GameDescription={'Pop colorful bubbles as they float across the screen!'} GamePhoto={GameCard3} Tag1={'Visual'} Tag2={'Sound'}></GameCard>
       
