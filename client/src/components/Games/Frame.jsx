5d287671b23563e711a67b28b08b75bd69d4d252
IllangasingheIMDP <157776370+IllangasingheIMDP@users.noreply.github.com>
2024-09-21 08:52:39 +0530

    Games tags updated
    get results by clicking tags

--- client/src/components/Games/Frame.jsx
+++ client/src/components/Games/Frame.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Frame.css";
 
-export const Frame = ({TagName}) => {
+export const Frame = ({TagName,setSelectedTag}) => {
     return (
-        <div className="frame">
-            <div className="text-wrapper">{TagName}</div>
-        </div>
+        
+        <button onClick={()=>{ TagName==='All Games' ? setSelectedTag(null) :setSelectedTag(TagName)}} className="frame tag py-0.5 px-2 w-full text-center text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 rounded-2xl border border-neutral-500">
+        {TagName}
+         </button>
     );
 };
--- client/src/components/Games/GameView.jsx
+++ client/src/components/Games/GameView.jsx
@@ -1,19 +1,53 @@
-import React from "react";
+import React,{useState} from "react";
 import { GameCard } from "./GameCard";
 
 import "./GameView.css";
 import GameCard1 from '../../assets/GameCard1.png'
 import GameCard2 from '../../assets/GameCard2.png'
 import GameCard3 from '../../assets/GameCard3.png'
-export const GameView = () => {
+export const GameView = ({selectedTag,setSelectedTag}) => {
+   
+
+
+    const games=[
+        {
+            GameName:'Color Safari',
+            GameDescription:'Join and color animals and feed them',
+            GamePhoto:GameCard1,
+            Tag1:'Visual',
+            Tag2:'Pattern Recognition'
+        },
+        {
+            GameName:'Scribbler Splash',
+            GameDescription:'Unleash your creativity with vibrant strokes and playful textures on a blank canvas!',
+            GamePhoto:GameCard2,
+            Tag1:'Creativity',
+            Tag2:'Fine Motor Skills'
+        },
+        {
+            GameName:'Bubble Pop',
+            GameDescription:'Pop colorful bubbles as they float across the screen!',
+            GamePhoto:GameCard3,
+            Tag1:'Visual',
+            Tag2:'Sound'
+        }
+
+
+    ]
+    const filteredGames=selectedTag ? games.filter(game=>game.Tag1===selectedTag || game.Tag2===selectedTag) : games
+
     return (
+        
         <div id='card-area'>
   <div className='wrapper '>
     
-      <GameCard GameName={'Color Safari'} GameDescription={'Join and color animals and feed them'} GamePhoto={GameCard1} Tag1={'Visual'} Tag2={'Pattern Recognition'}></GameCard>
+      {/* <GameCard GameName={'Color Safari'} GameDescription={'Join and color animals and feed them'} GamePhoto={GameCard1} Tag1={'Visual'} Tag2={'Pattern Recognition'}></GameCard>
       <GameCard GameName={'Scribbler Splash'} GameDescription={'Unleash your creativity with vibrant strokes and playful textures on a blank canvas!'} GamePhoto={GameCard2} Tag1={'Creativity'} Tag2={'Fine Motor Skills'}></GameCard>
       <GameCard GameName={'Bubble Pop'} GameDescription={'Pop colorful bubbles as they float across the screen!'} GamePhoto={GameCard3} Tag1={'Visual'} Tag2={'Sound'}></GameCard>
-      
+       */}
+       {filteredGames.map((game,index)=>(
+        <GameCard key={index} GameName={game.GameName} GameDescription={game.GameDescription} GamePhoto={game.GamePhoto} Tag1={game.Tag1} Tag2={game.Tag2}  onTagClick={setSelectedTag}></GameCard>
+       ))}
      
       
 
